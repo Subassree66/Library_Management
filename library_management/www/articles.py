e5b05a67816137715ce9cb5a4c8e664b43a5038e
Subassree66 <subassree.6616@gmail.com>
2026-09-11 10:05:11 +0000

    Guide 3 assignment article doctype and test class created

---
+++ library_management/www/articles.py
@@ -0,0 +1,9 @@
+import frappe
+
+
+def get_context(context):
+    context.articles = frappe.get_all(
+        "Article",
+        filters={"status": "Published"},
+        fields=["title", "name"]
+    )
